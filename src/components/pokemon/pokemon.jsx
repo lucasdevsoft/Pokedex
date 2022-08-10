9b94201ae35b8f397802644c01382381bd8dc733
Lucas Garcia <106991248+lucasdevsoft@users.noreply.github.com>
2022-08-10 19:49:45 -0300

    Add files via upload

--- src/components/pokemon/pokemon.jsx
+++ src/components/pokemon/pokemon.jsx
@@ -3,16 +3,18 @@
 export const Pokemon = (props) => {
     const { pokemon } = props;
 
+
     return (
-        <div className="m-3">
-                <div id="card" className="card">
-                    <div className="card-img-top">
-                        <img src={pokemon.sprites.front_default} className="card-img" alt={pokemon.name} />
-                    </div>
+        <>
+            <div className="m-3">
+            <div id="card" className="card">
+                <div id="bckgd-img" className={`${pokemon.types[0].type.name}`}>
+                    <img src={pokemon.sprites.other.dream_world.front_default} className="card-img" alt={pokemon.name} />
+                </div>
                     <div id="card-body" className="card-body ">
                         <p id="id" className='card-title'>N.°{(pokemon.id + 1).toString().padStart(3, 0)}</p>
                         <h4 className="card-title">{pokemon.name.replace(/\b\w/g, l => l.toUpperCase())}</h4>
-                        <div className="card-bottom">
+                    <div className="card-bottom">
                             <div className='card-type'>
                                 {
                                     pokemon.types.map((type, idx) => {
@@ -20,10 +22,13 @@ export const Pokemon = (props) => {
                                     })
                                 }
                             </div>
-                        </div>
+                    </div>
                 </div>
+            </div>    
             </div>
-        </div>
+        </>
+
+        
         
         
     )
